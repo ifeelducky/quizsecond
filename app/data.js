9b4746b8cce1215084a86028eafcc552921c162d
kvamvasa <cathringo@gmail.com>
2024-11-02 16:19:16 +0200

    basic quiz

---
+++ app/data.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    totalQuestions:5,
+    questions: [
+        {
+            id:1,
+            question: 'What is the right answer 1?',
+            answers: ['wrong 1', 'right', 'wrong 2','wrong 3'],
+            correctAnswer: 'right'
+        },
+        {
+            id:1,
+            question: 'What is the right answer 1?',
+            answers: ['wrong 1', 'right', 'wrong 2','wrong 3'],
+            correctAnswer: 'right'
+        },
+        {
+            id:3,
+            question: 'What is the right answer 3?',
+            answers: ['wrong 1', 'right', 'wrong 2','wrong 3'],
+            correctAnswer: 'right'
+        },
+        {
+            id:4,
+            question: 'What is the right answer 4?',
+            answers: ['wrong 1', 'right', 'wrong 2','wrong 3'],
+            correctAnswer: 'right'
+        },
+        {
+            id:5,
+            question: 'What is the right answer 5?',
+            answers: ['wrong 1', 'right', 'wrong 2','wrong 3'],
+            correctAnswer: 'right'
+        },
+
+    ]
+}
